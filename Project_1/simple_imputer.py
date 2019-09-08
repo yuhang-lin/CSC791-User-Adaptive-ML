2e195bcb5239dc45ea6979ce5f7665a03ca5e9fd
Yhang Lin <ylin34@ncsu.edu>
2019-09-07 21:12:40 -0400

    Add function impute_df as basic method for imputing data

--- Project_1/simple_imputer.py
+++ Project_1/simple_imputer.py
@@ -5,25 +5,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
-import os
+from base import impute_df
 
-
+strategy = 'mean'
 data_folder = './train_data'
-output_folder = "./output/simple_imputer"
-try:
-    os.makedirs(output_folder)
-except FileExistsError:
-    # directory already exists
-    pass
+output_folder = "./output/simple_imputer_{}".format(strategy)
 
-patients = pd.read_csv("{}/pts.tr.csv".format(data_folder), header=None)
-
-for patient in patients.iloc[:, 0]:
-    input_df = pd.read_csv("{}/train_with_missing/{}.csv".format(data_folder, patient))
-    fill_NaN = SimpleImputer(missing_values=np.nan, strategy='mean')
-    imputed_df = pd.DataFrame(fill_NaN.fit_transform(input_df))
-    imputed_df.columns = input_df.columns
-    imputed_df.index = input_df.index
-    imputed_df.to_csv("{}/{}.csv".format(output_folder, patient), index=False)
-    del input_df
-    del imputed_df
+imputer = SimpleImputer(missing_values=np.nan, strategy=strategy)
+impute_df(imputer, output_folder, data_folder)
