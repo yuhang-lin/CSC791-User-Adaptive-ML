553d7f79834252151fa0b96e27b2e25ffa96fe1d
Kunal <kunalnarang1994@gmail.com>
2019-09-15 09:43:34 -0400

    Added summary stats outputs to text file

--- Project_1/summary_stats.py
+++ Project_1/summary_stats.py
@@ -2,7 +2,11 @@ import pandas as pd
 import numpy as np
 
 # To switch between ground truth and missing data, please change the 'data' parameter to the function below
-def summary_per_analyte(data_folder = './train_data', data = '/train_groundtruth/'):
+def summary_per_analyte(data_choice, data_folder = './train_data'):
+    if data_choice == 'G':
+        data = '/train_groundtruth/'
+    else:
+        data = '/train_with_missing/'
     patients = pd.read_csv("{}/pts.tr.csv".format(data_folder), header=None)
     first_patient = pd.read_csv("{}{}{}.csv".format(data_folder, data, 1))
 
@@ -16,6 +20,8 @@ def summary_per_analyte(data_folder = './train_data', data = '/train_groundtruth
         df = pd.read_csv("{}{}{}.csv".format(data_folder, data, patient))
         length_list.append(df.shape[0])  # appending to list of number of rows
 
+
+
     # Initialize a 2D array with zeroes which holds count of missing values per patient per analyte
     count = np.zeros((len(length_list), column_length))
 
@@ -28,12 +34,14 @@ def summary_per_analyte(data_folder = './train_data', data = '/train_groundtruth
             count[i][j] = df.iloc[:, j].isnull().sum()  # Count
             summation[j] += count[i][j]
 
-    print('Counts of missing value per analyte over all patients is: {}'.format(summation))
-    print('Average number of missing values per analyte is: {}'.format(np.mean(summation)))
-    print('SD of number of missing values per analyte is: {}'.format(np.std(summation)))
-    print('Mean number of rows over all patients is: {}'.format(np.mean(length_list)))
-    print('Median number of rows over all patients is: {}'.format(np.median(length_list)))
-    print('SD of number of rows over all patients is: {}'.format(np.std(length_list)))
+    f = open('{}_output.txt'.format(data.strip('/')), 'w+')
+    f.write('Counts of missing value per analyte over all patients is: {}\n'.format(summation))
+    f.write('Average number of missing values per analyte is: {}\n'.format(np.mean(summation)))
+    f.write('SD of number of missing values per analyte is: {}\n'.format(np.std(summation)))
+    f.write('Mean number of rows over all patients is: {}\n'.format(np.mean(length_list)))
+    f.write('Median number of rows over all patients is: {}\n'.format(np.median(length_list)))
+    f.write('SD of number of rows over all patients is: {}\n'.format(np.std(length_list)))
+    f.close()
 
 
 
@@ -52,5 +60,7 @@ def patients_stats(data_folder = './train_data'):
     print(masked_value)
 
 
+# Choose between 'G' for train_groundtruth and 'M' for train_with_missing
+summary_per_analyte(data_choice='M')
 
-patients_stats()
+# patients_stats()
