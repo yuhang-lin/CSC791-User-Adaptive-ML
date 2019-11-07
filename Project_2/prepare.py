d69dd125e35906cd86d46e431b9618f98ba2251b
Yhang Lin <ylin34@ncsu.edu>
2019-11-07 18:40:21 -0500

    Fixed bug of unable to move the output files to output directory

--- Project_2/prepare.py
+++ Project_2/prepare.py
@@ -6,17 +6,23 @@ import os
 import pandas as pd
 
 
-def prepare(feature_list, output_folder=".", data_folder="."):
-    """
-    Prepare a CSV file "Training_data.csv" with static information along with selected features.
-    """
-    output_folder="."
-    data_folder="."
+def prepare(feature_list, output_folder=".", data_folder=".", file_name="Training_data"):
+    # save selected features to a csv file
     os.makedirs(output_folder, exist_ok=True)
     df = pd.read_csv("{}/MDP_Original_data.csv".format(data_folder), dtype='str')
-    keep_list = [i for i in range(6)] # Keep columns 1-6 untouched
-    keep_list.extend(feature_list) # Add all selected features
-    df[df.columns[keep_list]].to_csv("{}/Training_data.csv".format(output_folder), index=False)
+    NUM_STATIC = 6
+    MAX_LENGTH = NUM_STATIC + 8 # up to eight features
+    keep_list = [i for i in range(NUM_STATIC)]
+    feature_list.sort()
+    for item in feature_list:
+        if item in keep_list:
+            continue
+        keep_list.append(item)
+    if len(keep_list) == NUM_STATIC:
+        return # we have no feature actually
+    if len(keep_list) > MAX_LENGTH:
+        print("Feature list has size over the maximum limit of {}".format(MAX_LENGTH - NUM_STATIC))
+    df[df.columns[keep_list]].to_csv("{}/{}.csv".format(output_folder, file_name), index=False)
 
 
 if __name__ == '__main__':
