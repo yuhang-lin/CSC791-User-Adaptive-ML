a9c0c3bd9238769b0119e3afe1e5e5cbf8e26e09
Yhang Lin <ylin34@ncsu.edu>
2019-11-12 17:14:32 -0500

    Change parameters

--- Project_2/prepare.py
+++ Project_2/prepare.py
@@ -6,10 +6,8 @@ import os
 import pandas as pd
 
 
-def prepare(feature_list, output_folder=".", data_folder=".", file_name="Training_data"):
-    # save selected features to a csv file
-    os.makedirs(output_folder, exist_ok=True)
-    df = pd.read_csv("{}/MDP_Original_data.csv".format(data_folder), dtype='str')
+def prepare(feature_list, source_file, output_file="Training_data.csv"):
+    df = pd.read_csv("{}".format(source_file), dtype='str')
     NUM_STATIC = 6
     MAX_LENGTH = NUM_STATIC + 8 # up to eight features
     keep_list = [i for i in range(NUM_STATIC)]
@@ -22,8 +20,9 @@ def prepare(feature_list, output_folder=".", data_folder=".", file_name="Trainin
         return # we have no feature actually
     if len(keep_list) > MAX_LENGTH:
         print("Feature list has size over the maximum limit of {}".format(MAX_LENGTH - NUM_STATIC))
-    df[df.columns[keep_list]].to_csv("{}/{}.csv".format(output_folder, file_name), index=False)
+        keep_list = keep_list[:MAX_LENGTH] # discard all the extra features
+    df[df.columns[keep_list]].to_csv("{}".format(output_file), index=False)
 
 
 if __name__ == '__main__':
-    prepare([16, 17])
+    prepare([16, 17], "MDP_Original_data") # create a file similar to sample_training_data.csv
