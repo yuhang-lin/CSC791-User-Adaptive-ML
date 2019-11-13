a6fb11f7f6ba9bd076d842944e8c5000bdfe48e0
Yhang Lin <ylin34@ncsu.edu>
2019-11-12 21:06:20 -0500

    Remember and check the history

--- Project_2/genetic.py
+++ Project_2/genetic.py
@@ -5,26 +5,33 @@ import random
 import MDP_policy
 from prepare import prepare
 from exportCSV import exportCSV
+import os
 
-num_feature = 6 # number of features
-num_generation = 10 # number of generation
-num_child = 10 # number of children in one generation
-use_ECR = True # selection based on ECR if true
-num_mutate = 1 # number of features being mutated at a time
+def get_history(num_feature):
+    hist = dict()
+    hist_file = "{}.csv".format(num_feature)
+    if not os.path.isfile(hist_file):
+        return hist
+    with open(hist_file) as fin:
+        for line in fin:
+            parts = line.strip().split(",")
+            hist[",".join(parts[2:])] = [float(parts[0]), float(parts[1])]
+    return hist
 
-
-# all possible features indices you can choose
-feature_list = [i for i in range(6, 130)]
-# get random initial policy
-parent = sorted(random.sample(feature_list, num_feature))
-
-def get_value(features, use_ECR):
-    filename = "temp.csv"
-    prepare(features, "binned_org_order.csv", filename)
-    ECR_value, IS_value = MDP_policy.induce_policy_MDP(filename)
-    data = [ECR_value, IS_value]
-    data.extend(features)
-    exportCSV(data, "{}.csv".format(num_feature))
+def get_value(hist, features, use_ECR):
+    ECR_value = None
+    IS_value = None
+    key = ",".join([str(i) for i in features])
+    if key in hist:
+        ECR_value = hist[key][0]
+        IS_value = hist[key][1]
+    else:
+        filename = "temp.csv"
+        prepare(features, "binned_org_order.csv", filename)
+        ECR_value, IS_value = MDP_policy.induce_policy_MDP(filename)
+        data = [ECR_value, IS_value]
+        data.extend(features)
+        exportCSV(data, "{}.csv".format(len(features)))
     curr_value = ECR_value
     if not use_ECR:
         curr_value = IS_value
@@ -41,22 +48,41 @@ def mutate(parent, feature_list, num_mutate):
         child_features[i] = new_features[i]
     return sorted(child_features)
 
-parent_value = get_value(parent, use_ECR)
-for i in range(num_generation):
-    best_value = None
-    best_child = None
-    for j in range(num_child):
-        child_features = mutate(parent, feature_list, num_mutate)
-        # prepare CSV based on the selected features
-        curr_value = get_value(child_features, use_ECR)
-        # get the best ECR or IS among all children
-        if best_value is None or best_value < curr_value:
-            best_value = curr_value
-            best_child = child_features
-    # compare it with the parent
-    if best_value > parent_value:
-        parent_value = best_value
-        parent = best_child
+def main(num_feature=8, num_generation=10, use_ECR=True, num_child=10, num_mutate=2):
+    """
+    num_feature # number of features
+    num_generation # number of generation
+    use_ECR # selection based on ECR if true
+    num_child # number of children in one generation
+    num_mutate # number of features being mutated at a time
+    """
+    num_feature = max(min(num_feature, 8), 1) # set a max value of 8 and min value of 1
+    # all possible features indices you can choose
+    feature_list = [i for i in range(6, 130)]
 
-print(parent)
+    # get random initial policy
+    parent = sorted(random.sample(feature_list, num_feature))
+
+    hist = get_history(num_feature)
+    parent_value = get_value(hist, parent, use_ECR)
+    for i in range(num_generation):
+        best_value = None
+        best_child = None
+        for j in range(num_child):
+            child_features = mutate(parent, feature_list, num_mutate)
+            # prepare CSV based on the selected features
+            curr_value = get_value(hist, child_features, use_ECR)
+            # get the best ECR or IS among all children
+            if best_value is None or best_value < curr_value:
+                best_value = curr_value
+                best_child = child_features
+        # compare it with the parent
+        if best_value > parent_value:
+            parent_value = best_value
+            parent = best_child
+    print(parent)
+    if use_ECR:
+        print("Best ECR value: {}".format(parent_value))
+    else:
+        print("Best IS value: {}".format(parent_value))
 
