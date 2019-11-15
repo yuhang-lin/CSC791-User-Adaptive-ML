4aac239579a8f2b49d8bfc7100d1a417bdf32c4e
Yhang Lin <ylin34@ncsu.edu>
2019-11-14 22:58:25 -0500

    Use latest API of genetic

--- Project_2/repeat.py
+++ Project_2/repeat.py
@@ -1,8 +1,12 @@
-import genetic
+from genetic import Genetic
 
 def repeat(length, numRound=20):
+    genetic = Genetic()
     for i in range(numRound):
         genetic.main(length)
 
 if __name__ == "__main__":
     repeat(8, 20)
+    repeat(7, 20)
+    repeat(6, 20)
+    repeat(5, 20)
