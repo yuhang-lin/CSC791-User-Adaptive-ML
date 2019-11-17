f89cb504189853f7b8bfb544038be4e1ea5a71a9
Kunal Narang <kunalnarang1994@gmail.com>
2019-11-17 16:15:52 -0500

    Added requirements

--- Project_2/correlation/correlation.py
+++ Project_2/correlation/correlation.py
@@ -4,24 +4,30 @@ import matplotlib.pyplot as plt
 
 def find_correlation(file, corr_op):
     df = pd.read_csv(file)
-    best_columns = df.iloc[:, [76, 77, 88, 102, 110, 111, 121, 128]]
-    print(best_columns)
+    best_columns = df.iloc[:, [18, 42, 85, 114]]
+    # print(best_columns)
 
     # calculate the correlation matrix
     corr = best_columns.corr()
     corr.to_csv(corr_op)
 
     # plot the heatmap
-    plt.suptitle('correlation between different features')
+    plt.suptitle('correlation between different features', fontsize=16)
     plt.autoscale()
     ax = sn.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, annot=True)
+    plt.xticks(rotation=20)
     bottom, top = ax.get_ylim()
     ax.set_ylim(bottom + 0.5, top - 0.5)
-    plt.savefig('corr_plot_4.png')
+
+    # for tick in ax.get_xticklabels():
+    #     tick.set_rotation(90)
+
+
+    plt.savefig('corr_plot_4b_4f.png')
     plt.show()
 
 def main():
-    find_correlation(file = '../binned_4_reorder.csv', corr_op = 'corr_output_4.csv')
+    find_correlation(file = '../binned_4_reorder.csv', corr_op = 'corr_output_4b_4f.csv')
 
 
 if __name__ == '__main__':
