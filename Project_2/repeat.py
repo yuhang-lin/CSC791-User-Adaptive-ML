2bc91b42114b2a7419b790023fae1897e25db19c
Yhang Lin <ylin34@ncsu.edu>
2019-11-17 16:33:52 -0500

    Add update of default number of rounds

--- Project_2/repeat.py
+++ Project_2/repeat.py
@@ -1,12 +1,11 @@
 from genetic import Genetic
 
 def repeat(length, numRound=20):
-    genetic = Genetic(2)
+    genetic = Genetic(4)
     for i in range(numRound):
         genetic.main(length)
 
 if __name__ == "__main__":
-    repeat(8, 20)
-    repeat(7, 20)
-    repeat(6, 20)
-    repeat(5, 20)
+    repeat(4, 3)
+    repeat(5, 3)
+    repeat(6, 3)
