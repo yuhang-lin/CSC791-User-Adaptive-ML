461810c356b0617c6d0f5bba524b387fc70f9224
Yhang Lin <ylin34@ncsu.edu>
2019-11-30 20:48:15 -0500

    Import functions from preprocessEMG.py

--- SSE/preprocessing_with_LSTM.py
+++ SSE/preprocessing_with_LSTM.py
@@ -1,184 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import glob
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from preprocessEMG import train_valid_test_split, getXY
+
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-import seaborn as sn
+#import matplotlib.pyplot as plt
 import os
-import csv
 import numpy as np
 import tensorflow as tf
 from statistics import mean
 
 
-def normalize_df(df):
-    # the time column
-    tim = pd.DataFrame(df['time'])
-
-    # the class column
-    clas = pd.DataFrame(df['class'])
-
-    # drop time from the dataset so that we don't scale the timestamps
-    df_new = df.drop(['time', 'class'], axis=1)
-
-    # Get the headers from the list
-    headers = list(df_new)
-
-    scaler = StandardScaler()
-    scaled_values = pd.DataFrame(scaler.fit_transform(df_new), columns = headers, index = df_new.index)
-    
-    # concatenate the data after scaling it
-    df_updated = pd.concat([tim, clas, scaled_values], axis=1)
-    df_updated = df_updated.astype({'class': 'int32'})
-    return df_updated
-
-# normal_data = []
-# folder = "normalized"
-# os.makedirs(folder, exist_ok=True)
-# for i, df in enumerate(user_data):
-#     new_df = df[df["class"] < 7]
-#     #new_df = df[df["class"] > 0]
-#     normalized_df = normalize_df(new_df)
-#     #normalized_df.to_csv("./{}/{}.csv".format(folder, i + 1))
-#     normal_data.append(normalized_df)
-
-def get_square_df(df):
-    new_df = df.pow(2)
-    # the time column
-    tim = pd.DataFrame(df['time'])
-
-    # the class column
-    clas = pd.DataFrame(df['class'])
-
-    # drop time from the dataset so that we don't scale the timestamps
-    new_df.drop(['time', 'class'], axis=1, inplace=True)
-
-    # concatenate the data after scaling it
-    new_df = pd.concat([tim, clas, new_df], axis=1)
-    new_df = new_df.astype({'class': 'int32'})
-    return new_df
-
-def RMS(df, start, end):
-    return df.iloc[start:end + 1, :].mean()
-
-def partition_window(df, window_size=200, step_size=100):
-    count = 0
-    rows, columns = df.shape
-    last_data = []
-    start = 0
-    end = 0
-    square_df = get_square_df(df)
-    RMS_list = []
-    window_list = []
-    
-    for i in range(0, rows, step_size):
-        if end > 0:
-            start = end - step_size + 1
-        else:
-            start = i
-        end = min(i + window_size - 1, rows - 1)
-        #count += 1 # count of total possible windows we can have
-        #print("{} to {}".format(start, end))
-        res = RMS(square_df, start, end)
-        if res[1] == int(res[1]) and int(res[1]) > 0 and int(res[1]) < 7 and end - start + 1 == window_size:
-            RMS_list.append(res)
-            window_list.append([start, end])
-        if end - start + 1 < window_size:
-            count += 1
-        if end == rows - 1:
-            break
-    return window_list, RMS_list, count
-
-# print("Total: {}, left: {}, lost percentage: {}".format(total_window, count, (1 - count/total_window) * 100 ))
-
-# print("Total: {}, left: {}, lost percentage: {}".format(total_window, count, (1 - count/total_window) * 100 ))
-
-# new_df = df.pow(2)
-# # the time column
-# tim = pd.DataFrame(df['time'])
-
-# # the class column
-# clas = pd.DataFrame(df['class'])
-
-# # drop time from the dataset so that we don't scale the timestamps
-# new_df.drop(['time', 'class'], axis=1, inplace=True)
-
-# # concatenate the data after scaling it
-# new_df = pd.concat([tim, clas, new_df], axis=1)
-# new_df = new_df.astype({'class': 'int32'})
-# res = new_df.iloc[start:end + 1, :].mean()
-
-def exportCSV(data, fileName='output.csv'):
-    """
-    data is a list storing the data to be appended to the csv fileName
-    """
-    with open(fileName, 'a+', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=',',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(data)
-
-# train, test split
-def train_valid_test_split(path = './EMG_data_for_gestures-master/'):
-    test_file = 1 # 1 for using the first half of the second text file
-    num_subject = 36 # number of subjects we have 
-    # find all the changing points first, 
-    # then use the first changing point from 6 to 0 minus 1 to be the row to split.
-    # split the data into train and test, then return list of dataframes
-    # a list holding the data for all 36 users
-    log_file = path + '{}.csv'.format("change_points")
-    change_points = [[] for i in range(num_subject)]
-    is_loaded = False
-    if os.path.isfile(log_file):
-        # load the data
-        count = 0
-        with open(log_file, 'r') as fin:
-            for line in fin:
-                count += 1
-                if count % 2 == 1:
-                    continue
-                data = list(map(int, line.strip().split(",")))
-                change_points[(count - 1) // 4].append(data)
-        is_loaded = True
-        # for i in range(num_subject):
-        #     print(change_points[i])
-
-    training = []
-    validation = []
-    testing = []
-    for i in range(num_subject):  # since there are 36 users
-        all_files = glob.glob(path + "{}/*.txt".format(i+1))
-        # read both files for each user
-        for j, filename in enumerate(all_files):
-            df = pd.read_csv(filename, sep = "\t")
-            if not is_loaded:
-                change_points[i].append([])
-                last_class = 0
-                classes = []
-                for index, row in enumerate(df.itertuples(index=False)):
-                    if row[-1] != last_class:
-                        last_class = row[-1]
-                        classes.append(last_class)
-                        change_points[i][j].append(index)
-                #print(classes)
-                exportCSV(classes, log_file)
-                #print(change_points[i][j])
-                exportCSV(change_points[i][j], log_file)
-            if j == test_file:
-                #print("{}, {}".format(i, j))
-                #print(change_points[i][j])
-                split_point = change_points[i][j][11]
-                # append first half to validation,
-                # second half to testing
-                validation.append(df.iloc[:split_point,:])
-                testing.append(df.iloc[split_point:,:])
-            else:
-                # append this whole df to training
-                training.append(df)
-    return training, validation, testing
 
 training, validation, testing = train_valid_test_split()
 
@@ -227,17 +59,6 @@ def evaluate_model(X_train, y_train, X_test, y_test):
     f1 = f1_score(y_test, y_pred, average="macro")
 
     return [acc, f1]
-
-def getXY(df):
-    # normalize this df
-    df = normalize_df(df)
-    # partition this df into windows
-    window_list, RMS_list, count = partition_window(df)
-    # get the X and y from RMS list
-    rmsX = [list(i[2:]) for i in RMS_list]
-    dataX = [df.iloc[window[0]:window[1] + 1,2:] for window in window_list]
-    y = [i[1] for i in RMS_list]
-    return rmsX, dataX, y
 
 
 results = []
