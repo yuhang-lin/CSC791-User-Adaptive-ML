668b048912b855575dbc46b343d5d3f008b93265
Yhang Lin <ylin34@ncsu.edu>
2019-12-01 22:49:37 -0500

    Run experiment 2

--- SSE/preprocessing_with_LSTM.py
+++ SSE/preprocessing_with_LSTM.py
@@ -86,7 +86,7 @@ def evaluate_model(subject, X_train, y_train, X_valid, y_valid, X_test, y_test, 
     
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.LSTM(100, input_shape=(n_timesteps,n_features)))    
-    model.add(tf.keras.layers.Dropout(0.3))
+    model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(100, activation='relu'))
     model.add(tf.keras.layers.Dense(n_outputs, activation='softmax'))
     model.compile(loss=tf.keras.losses.sparse_categorical_crossentropy, optimizer=tf.keras.optimizers.Adam(), 
@@ -114,7 +114,7 @@ def evaluate_model(subject, X_train, y_train, X_valid, y_valid, X_test, y_test, 
 
 
 results = []
-epochs = 100
+epochs = 200
 for i in range(36):
     print("----------------------\n")
     print("Training for user {}\n".format(i+1))
