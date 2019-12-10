17f884a51cb953355ecc1ca5735c50d1db92d1c4
Yhang Lin <ylin34@ncsu.edu>
2019-12-10 16:25:22 -0500

    Add model for individual training

--- SSE/cnnStackedLSTM.py
+++ SSE/cnnStackedLSTM.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
-os.environ['PYTHONHASHSEED'] = '0'
-os.environ['CUDA_VISIBLE_DEVICES']='-1'
-os.environ['TF_CUDNN_USE_AUTOTUNE'] ='0'
+# import os
+# os.environ['PYTHONHASHSEED'] = '0'
+# os.environ['CUDA_VISIBLE_DEVICES']='-1'
+# os.environ['TF_CUDNN_USE_AUTOTUNE'] ='0'
 
-import numpy as np
-import random as rn
-import tensorflow as tf
+# import numpy as np
+# import random as rn
+# import tensorflow as tf
 
-rn.seed(1)
-np.random.seed(1)
-from tensorflow import set_random_seed
-set_random_seed(1)
+# rn.seed(1)
+# np.random.seed(1)
+# from tensorflow import set_random_seed
+# set_random_seed(1)
 
-from keras import backend as k
-config = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1,
-allow_soft_placement=True, device_count = {'CPU': 1})
-sess = tf.Session(graph=tf.get_default_graph(),config=config)
-k.set_session(sess)
+# from keras import backend as k
+# config = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1,
+# allow_soft_placement=True, device_count = {'CPU': 1})
+# sess = tf.Session(graph=tf.get_default_graph(),config=config)
+# k.set_session(sess)
 
 from preprocessEMG import train_valid_test_split, getXY
 from evaluateBase import generate_plots
@@ -38,19 +38,24 @@ from keras.layers.convolutional import Conv1D, Conv2D, MaxPooling1D, MaxPooling2
 from keras.optimizers import Adam
 from keras.regularizers import l2
 
-def build_model(n_length, n_features, n_outputs):
+def build_model(n_length, n_features, n_outputs, individual_training=False):
     model = Sequential()
-    #model.add(TimeDistributed(Conv2D(64, (3, 3), strides=(2, 2), activation='relu'), input_shape=(None, n_length, n_features)))
-    #model.add(TimeDistributed(MaxPooling2D(pool_size=2)))
-    model.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu', kernel_regularizer=l2(0.01)), input_shape=(None, n_length, n_features)))
+    model.add(TimeDistributed(Conv1D(filters=64, kernel_size=3, activation='relu', kernel_regularizer=l2(0.05)), 
+                              input_shape=(None, n_length, n_features)))
     model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
-    model.add(TimeDistributed(Flatten()))
-    model.add(LSTM(50, activation='relu', recurrent_regularizer=l2(0.01), return_sequences=True))
-    #model.add(Dropout(0.5))
-    model.add(LSTM(50, activation='relu', recurrent_regularizer=l2(0.01)))
-    #model.add(Dropout(0.5))
-    model.add(Dense(32, activation='relu', kernel_regularizer=l2(0.01)))
-    #model.add(Dropout(0.5))
+    model.add(TimeDistributed(Flatten())) 
+    if individual_training:
+        model.add(LSTM(50, activation='relu', return_sequences=True))
+        model.add(LSTM(50, activation='relu'))
+        model.add(Dropout(0.1))
+        model.add(Dense(32, activation='relu'))
+        model.add(Dropout(0.1))
+    else:
+        model.add(LSTM(50, activation='tanh', recurrent_regularizer=l2(0.05), return_sequences=True))
+        model.add(Dropout(0.5))
+        model.add(LSTM(50, activation='tanh', recurrent_regularizer=l2(0.01)))
+        model.add(Dropout(0.5))
+        model.add(Dense(32, activation='relu', kernel_regularizer=l2(0.01)))
     model.add(Dense(n_outputs, activation='softmax'))
     opt = Adam()
     model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
@@ -69,13 +74,11 @@ def test_model(subject, model, X_test, y_test):
 
     acc = accuracy_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred, average="macro")
-    
-    #generate_plots(subject, hist)
 
     return [acc, f1]
 
 # fit and evaluate a model
-def train_model(subject, X_train, y_train, X_valid, y_valid, epochs=50):
+def train_model(subject, X_train, y_train, X_valid, y_valid, epochs=50, individual_training=False):
     """A function that trains an LSTM model and returns the accuracy on the test dataset.
 
     Parameters
@@ -103,21 +106,17 @@ def train_model(subject, X_train, y_train, X_valid, y_valid, epochs=50):
     es = EarlyStopping(monitor='val_loss', mode='min', patience=7, verbose=1, restore_best_weights=True)
     #es = EarlyStopping(monitor='val_acc', mode='max', min_delta=0.001, patience=15, verbose=1, restore_best_weights=True)
     #mcp_save = ModelCheckpoint('.mdl_wts.hdf5', monitor='val_accuracy', mode='max', save_best_only=True)
-    mcp_save = ModelCheckpoint('.mdl_wts.hdf5', monitor='val_loss', mode='min')
+    mcp_save = ModelCheckpoint('mdl_wts.hdf5', monitor='val_loss', mode='min')
    
     X_train = X_train.reshape(X_train.shape[0], n_steps, n_length, n_features)
     X_valid = X_valid.reshape(X_valid.shape[0], n_steps, n_length, n_features)
     
     
-    model = build_model(n_length, n_features, n_outputs)
+    model = build_model(n_length, n_features, n_outputs, individual_training)
     model.summary()
     # fit network
-    hist = model.fit(X_train, y_train, shuffle=False, epochs=epochs, batch_size=batch_size, callbacks=[es, mcp_save], validation_data=(X_valid, y_valid))
+    hist = model.fit(X_train, y_train, shuffle=True, epochs=epochs, batch_size=batch_size, callbacks=[es, mcp_save], validation_data=(X_valid, y_valid))
     return model
-    
-#     print("printing keys in the hist")
-#     for key in hist.history:
-#         print(key)
 
 training, validation, testing = train_valid_test_split()
 results = []
@@ -130,9 +129,6 @@ if individual_training:
         print("----------------------\n")
         trainRMSX, trainX, trainY = getXY(training[i])
         validRMSX, validX, validY = getXY(validation[i])
-        # combine validatation and training together
-        #trainX.extend(validX)
-        #trainY.extend(validY)
 
         # get the testing data
         testRMSX, testX, testY = getXY(testing[i])
@@ -145,7 +141,7 @@ if individual_training:
         testY = np.asarray(testY)  
 
         # train and test the model
-        model = train_model(i + 1, trainX, trainY, validX, validY, epochs)
+        model = train_model(i + 1, trainX, trainY, validX, validY, epochs, individual_training)
         results.append(test_model(i + 1, model, testX, testY))
 else:
     combineTrainX = []
@@ -164,7 +160,7 @@ else:
     combineTrainY = np.asarray(combineTrainY)
     combineValidX = np.asarray([X.values for X in combineValidX])
     combineValidY = np.asarray(combineValidY)
-    model = train_model(i + 1, combineTrainX, combineTrainY, combineValidX, combineValidY, epochs)
+    model = train_model(i + 1, combineTrainX, combineTrainY, combineValidX, combineValidY, epochs, individual_training)
     for i in range(36):
         testRMSX, testX, testY = getXY(testing[i])
         testX = np.asarray([X.values for X in testX])
@@ -181,8 +177,12 @@ accuracies_2.extend(accuracies)
 f1_scores_2 = [experiment_name, mean(f1_scores), stdev(f1_scores)]
 f1_scores_2.extend(f1_scores)
 
-exportCSV(accuracies_2, "accuracy_cnnlstm.csv")
-exportCSV(f1_scores_2, "f1_cnnlstm.csv")
+if individual_training:
+    exportCSV(accuracies_2, "accuracy_cnnStackedLSTM_indiv.csv")
+    exportCSV(f1_scores_2, "f1_cnnStackedLSTM_indiv.csv")
+else:
+    exportCSV(accuracies_2, "accuracy_cnnStackedLSTM_agg.csv")
+    exportCSV(f1_scores_2, "f1_cnnStackedLSTM_indiv.csv")
 
 print(accuracies_2)
 print(f1_scores_2)
