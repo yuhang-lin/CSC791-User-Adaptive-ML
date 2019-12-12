62bbba26a8d7d8a8722b0692466afe2a98d5dcad
Yhang Lin <ylin34@ncsu.edu>
2019-12-11 22:04:52 -0500

    Use keras

--- SSE/LSTM.py
+++ SSE/LSTM.py
@@ -1,61 +1,50 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# coding: utf-8
 
 from preprocessEMG import train_valid_test_split, getXY
+from evaluateBase import generate_plots
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 import matplotlib.pyplot as plt
 import os
 import numpy as np
-import tensorflow as tf
-from statistics import mean
+from statistics import mean, stdev
 from keras.callbacks import LearningRateScheduler, EarlyStopping, ModelCheckpoint
 from exportCSV import exportCSV
+from keras.models import Sequential
+from keras.layers import Bidirectional, ConvLSTM2D, ConvLSTM2D, Dense, Dropout, Flatten, LSTM, TimeDistributed
+from keras.layers.convolutional import Conv1D, Conv2D, MaxPooling1D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.regularizers import l2
 
+def build_model(n_length, n_features, n_outputs, individual_training=False):
+    model = Sequential()
+    model.add(LSTM(100, input_shape=(n_length,n_features)))  
+    model.add(Dropout(0.3))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(n_outputs, activation='softmax'))
+    opt = Adam()
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+    return model
 
-training, validation, testing = train_valid_test_split()
+def test_model(subject, model, X_test, y_test, individual_training):
+    verbose, batch_size = 0, 32
+    window_size, n_features, n_outputs = 200, 8, 6
+    n_steps = 1
+    n_length = window_size // n_steps
 
-def generate_plots(subject, model_history):
-    """
-    A method that takes the model history of a trained model and plots its:
-    1. Training accuracy
-    2. Training loss
-    3. Validation accuracy
-    4. Validation loss
-    """
-    acc = model_history.history['acc']
-    val_acc = model_history.history['val_acc']
-    loss = model_history.history['loss']
-    val_loss = model_history.history['val_loss']
-    epochs = len(acc)
-    
-    plt.figure(1 + int(subject))
-    plt.suptitle('Accuracy learning curve', fontsize=20)
-    plt.xlabel('epochs', fontsize=14)
-    plt.ylabel('accuracy', fontsize=14)
-    plt.plot(acc, label='training accuracy')
-    plt.plot(val_acc, label='validation accuracy')
-    plt.xticks(np.arange(0, epochs, epochs/10))
-    plt.legend(loc="lower right")
-    os.makedirs("fig_lstm", exist_ok=True)
-    plt.savefig("fig_lstm/accuracy{}.png".format(subject), dpi=300)
-    
-    plt.figure(200 + int(subject))
-    plt.suptitle('Loss learning curve', fontsize=20)
-    plt.xlabel('epochs', fontsize=14)
-    plt.ylabel('loss', fontsize=14)
-    plt.plot(loss, label='training loss')
-    plt.plot(val_loss, label='validation loss')
-    plt.xticks(np.arange(0, epochs, epochs/10))
-    plt.legend(loc="upper right")
-    plt.savefig("fig_lstm/loss{}.png".format(subject), dpi=300)
-    #plt.show()
-    plt.close()
+    predicted_labels = model.predict(X_test, batch_size=batch_size)
 
+    y_pred = [np.argmax(x) for x in predicted_labels]
 
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average="macro")
+
+    return [acc, f1]
 
 # fit and evaluate a model
-def evaluate_model(subject, X_train, y_train, X_valid, y_valid, X_test, y_test, epochs=50):
+def train_model(subject, X_train, y_train, X_valid, y_valid, epochs=50, individual_training=False):
     """A function that trains an LSTM model and returns the accuracy on the test dataset.
 
     Parameters
@@ -75,77 +64,85 @@ def evaluate_model(subject, X_train, y_train, X_valid, y_valid, X_test, y_test, 
 
     """
     verbose, batch_size = 0, 32
-    n_timesteps, n_features, n_outputs = 200, 8, 6
+    window_size, n_features, n_outputs = 200, 8, 6
+    n_steps = 1
+    n_length = window_size // n_steps
+
+    es = EarlyStopping(monitor='val_loss', mode='min', patience=7, verbose=1, restore_best_weights=True)
+    mcp_save = ModelCheckpoint('mdl_wts.hdf5', monitor='val_loss', mode='min') 
     
-    #annealer = LearningRateScheduler(lambda x: 1e-3 * 0.95 ** (x + epochs))
-    #es = EarlyStopping(monitor='val_loss', mode='min', min_delta=0.001, patience=15, verbose=1, restore_best_weights=True)
-    #es = EarlyStopping(monitor='val_acc', mode='max', min_delta=0.001, patience=15, verbose=1, restore_best_weights=True)
-    mcp_save = ModelCheckpoint('.mdl_wts.hdf5', monitor='val_acc', mode='max', save_best_only=True)
-    #mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='min')
-   
-    
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.LSTM(100, input_shape=(n_timesteps,n_features)))    
-    model.add(tf.keras.layers.Dropout(0.3))
-    model.add(tf.keras.layers.Dense(100, activation='relu'))
-    model.add(tf.keras.layers.Dense(n_outputs, activation='softmax'))
-    model.compile(loss=tf.keras.losses.sparse_categorical_crossentropy, optimizer=tf.keras.optimizers.Adam(), 
-                  metrics=['accuracy'])
+    model = build_model(n_length, n_features, n_outputs, individual_training)
     model.summary()
     # fit network
-    
-    with tf.device('/device:GPU:0'):
-        history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, callbacks=[mcp_save], 
-                            validation_data=(X_valid, y_valid))
-        
-    # evaluate model
-    # _, accuracy = model.evaluate(X_test, y_test, batch_size=batch_size)
-    model.load_weights(filepath = '.mdl_wts.hdf5')
-    predicted_labels = model.predict(X_test, batch_size=batch_size)
+    hist = model.fit(X_train, y_train, shuffle=True, epochs=epochs, batch_size=batch_size, callbacks=[es, mcp_save], validation_data=(X_valid, y_valid))
+    return model
 
-    y_pred = [np.argmax(x) for x in predicted_labels]
-
-    acc = accuracy_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred, average="macro")
-    
-    generate_plots(subject, history)
-
-    return [acc, f1]
-
-
+training, validation, testing = train_valid_test_split()
 results = []
-epochs = 200
-for i in range(36):
-    print("----------------------\n")
-    print("Training for user {}\n".format(i+1))
-    print("----------------------\n")
-    trainRMSX, trainX, trainY = getXY(training[i])
-    validRMSX, validX, validY = getXY(validation[i])
-    # combine validatation and training together
-    #trainX.extend(validX)
-    #trainY.extend(validY)
+epochs = 160
+individual_training = False
+if individual_training:
+    for i in range(36):
+        print("----------------------\n")
+        print("Training for user {}\n".format(i+1))
+        print("----------------------\n")
+        trainRMSX, trainX, trainY = getXY(training[i])
+        validRMSX, validX, validY = getXY(validation[i])
 
-    # get the testing data
-    testRMSX, testX, testY = getXY(testing[i])
-    
-    trainX = np.asarray([X.values for X in trainX])
-    trainY = np.asarray(trainY)
-    validX = np.asarray([X.values for X in validX])
-    validY = np.asarray(validY)
-    testX = np.asarray([X.values for X in testX])
-    testY = np.asarray(testY)
-    
+        # get the testing data
+        testRMSX, testX, testY = getXY(testing[i])
 
-    # train and test the model
-    results.append(evaluate_model(i + 1, trainX, trainY, validX, validY, testX, testY, epochs))
-    
+        trainX = np.asarray([X.values for X in trainX])
+        trainY = np.asarray(trainY)
+        validX = np.asarray([X.values for X in validX])
+        validY = np.asarray(validY)
+        testX = np.asarray([X.values for X in testX])
+        testY = np.asarray(testY)  
+
+        # train and test the model
+        model = train_model(i + 1, trainX, trainY, validX, validY, epochs, individual_training)
+        results.append(test_model(i + 1, model, testX, testY, individual_training))
+else:
+    combineTrainX = []
+    combineTrainY = []
+    combineValidX = []
+    combineValidY = []
+    # combine all training data together
+    for i in range(36):
+        trainRMSX, trainX, trainY = getXY(training[i])
+        validRMSX, validX, validY = getXY(validation[i])
+        combineTrainX.extend(trainX)
+        combineTrainY.extend(trainY)
+        combineValidX.extend(validX)
+        combineValidY.extend(validY)
+    combineTrainX = np.asarray([X.values for X in combineTrainX])
+    combineTrainY = np.asarray(combineTrainY)
+    combineValidX = np.asarray([X.values for X in combineValidX])
+    combineValidY = np.asarray(combineValidY)
+    model = train_model(i + 1, combineTrainX, combineTrainY, combineValidX, combineValidY, epochs, individual_training)
+    for i in range(36):
+        testRMSX, testX, testY = getXY(testing[i])
+        testX = np.asarray([X.values for X in testX])
+        testY = np.asarray(testY)
+        results.append(test_model(i + 1, model, testX, testY, individual_training))
 
 accuracies = [val[0] for val in results]
 f1_scores = [val[1] for val in results]
 
-f1_scores.insert(0, "lstm")
-accuracies.insert(0, "lstm")
-exportCSV(f1_scores, "f1_micro_lstm.csv")
-exportCSV(accuracies, "accuracy_lstm.csv")
-print(f1_scores)
-print(accuracies)
+experiment_name = "LSTM"
+
+accuracies_2 = [experiment_name, mean(accuracies), stdev(accuracies)]
+accuracies_2.extend(accuracies)
+f1_scores_2 = [experiment_name, mean(f1_scores), stdev(f1_scores)]
+f1_scores_2.extend(f1_scores)
+
+if individual_training:
+    exportCSV(accuracies_2, "accuracy_LSTM_indiv.csv")
+    exportCSV(f1_scores_2, "f1_LSTM_indiv.csv")
+else:
+    exportCSV(accuracies_2, "accuracy_LSTM_agg.csv")
+    exportCSV(f1_scores_2, "f1_LSTM_agg.csv")
+
+print(accuracies_2)
+print(f1_scores_2)
+
